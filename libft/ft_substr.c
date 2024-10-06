dc885670f0a518672dc60ba2776a44285dadfca5
tayqiter00 <tayqiter00@gmail.com>
2024-10-06 16:33:20 +0800

    Add libft

---
+++ libft/ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qtay <qtay@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 17:46:34 by qtay              #+#    #+#             */
+/*   Updated: 2024/09/03 18:25:06 by qtay             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*buffer;
+	size_t	i;
+	size_t	s_len;
+
+	i = 0;
+	if (s == NULL)
+		exit(3);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > (s_len - start))
+		len = s_len - start;
+	buffer = (char *)malloc(sizeof(char) * (len + 1));
+	if (!buffer)
+		return (0);
+	while (start < s_len && i < len)
+		buffer[i++] = s[start++];
+	buffer[i] = '\0';
+	return (buffer);
+}
